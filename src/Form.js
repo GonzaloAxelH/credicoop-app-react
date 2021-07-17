87275b9350b6596c011f970ab7311ef2d5940a5d
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-17 14:54:56 -0500

    !add

---
+++ src/Form.js
@@ -0,0 +1,74 @@
+import React from 'react';
+ import { useFormik } from 'formik';
+ import './Form.css'
+ // A custom validation function. This must return an object
+ // which keys are symmetrical to our values/initialValues
+ const validate = values => {
+   const errors = {};
+   if (!values.codigo) {
+     errors.codigo = 'Required';
+   } else if (values.codigo.length > 10){
+     errors.codigo = 'Sobrepasa el codigo';
+   } else if( values.codigo.length < 10 && values.length > 0){
+
+     errors.codigo = 'Falta dijitos del codigo';
+   }
+ 
+   if (!values.password) {
+     errors.password = 'Required';
+   }
+    return errors;
+ };
+ 
+ const SignupForm = () => {
+   const formik = useFormik({
+     initialValues: {
+       codigo: '',
+       password: '',
+     },
+     validate,
+     onSubmit: values => {
+       alert(JSON.stringify(values, null, 2));
+     },
+   });
+   return (
+     <div className="container__info-form">
+       <h1 className="container__form-title">Ingresa tus datos aquí</h1>
+       <p className="container__form-subtitle">
+         Para abrir tu cuenta, debes tener nacionalidad peruana y 18 años o más.
+       </p>
+       
+     <form onSubmit={formik.handleSubmit} className="container__form">
+       
+       <div className={formik.errors.codigo ? "container__form-control active" : "container__form-control"} >
+          <label htmlFor="codigo">Codigo</label><br/>
+          <input
+            id="codigo"
+            name="codigo"					
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.codigo}	
+          />
+          {formik.errors.codigo ? <div className="form__message-error">{formik.errors.codigo}</div> : null}
+       </div>
+       
+       <div className="container__form-control">
+       <label htmlFor="password">Contraseña</label><br/>
+       <input
+         id="password"
+         name="password"
+         type="password"
+         onChange={formik.handleChange}
+         value={formik.values.password}
+       />
+       {formik.errors.password ? <div className="form__message-error">{formik.errors.password}</div> : null}
+      </div>
+    
+ 
+       <button  className="container__form-button" type="submit">Submit</button>
+     </form>
+   </div>
+   );
+ };
+
+ export default SignupForm;
